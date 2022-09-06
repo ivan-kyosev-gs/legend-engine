93ce44e3927ccf11a30e4ae4f7d052289f709ebc
Ivan Kyosev <89931850+ivan-kyosev-gs@users.noreply.github.com>
2022-09-06 19:12:48 +0100

    Updating script

--- .gcp/release.sh
+++ .gcp/release.sh
@@ -20,7 +20,7 @@ export MAVEN_OPTS=-Xmx4g
 # importing GPG private key
 echo "========================================================================="
 echo "${CI_GPG_PRIVATE_KEY}" > private.key
-gpg --batch --pinentry-mode loopback --import private.key
+gpg --batch --import private.key
 rm private.key
 echo "========================================================================="
 gpg --list-secret-keys
