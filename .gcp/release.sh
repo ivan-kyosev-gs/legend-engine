ecc2051dce80c6f1f2e6755b02f0f682763a33fb
Ivan Kyosev <89931850+ivan-kyosev-gs@users.noreply.github.com>
2022-09-06 13:36:04 +0100

    Using maven-openjdk container

--- .gcp/release.sh
+++ .gcp/release.sh
@@ -2,7 +2,11 @@
 
 echo "Provided release version is: ${RELEASE_VERSION}"
 
+echo "JAVA_HOME is: ${JAVA_HOME}"
+echo "M2_HOME is: ${M2_HOME}"
 
+java --version
+mvn --version
 
 
 # export MAVEN_OPTS=-Xmx12g
