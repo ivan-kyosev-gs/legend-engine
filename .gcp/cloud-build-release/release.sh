af2f69a8c63901f6466c406eca4ccf6b32a7ee13
Ivan Kyosev <89931850+ivan-kyosev-gs@users.noreply.github.com>
2022-09-15 20:50:22 +0100

    Updating script

--- .gcp/cloud-build-release/release.sh
+++ .gcp/cloud-build-release/release.sh
@@ -13,7 +13,7 @@ apt-get update && apt-get install -y ca-certificates curl gnupg lsb-release
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
-apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+apt-get update && apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 
 echo "--------------------------------------- DOCKER VERSION ----------------------------------"
