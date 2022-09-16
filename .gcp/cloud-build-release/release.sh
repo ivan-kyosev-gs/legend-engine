dac8ab1c75e9a85d132624a03c8c8fe5057eb1d1
Ivan Kyosev <89931850+ivan-kyosev-gs@users.noreply.github.com>
2022-09-16 13:37:42 +0100

    Removing docker installation

--- .gcp/cloud-build-release/release.sh
+++ .gcp/cloud-build-release/release.sh
@@ -8,28 +8,28 @@ mvn --version
 gpg --version
 
 # installing docker
-apt update && apt -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common
-curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/trusted.gpg.d/docker-archive-keyring.gpg
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-apt update && apt -y install docker-ce docker-ce-cli containerd.io
-systemctl enable --now docker
-usermod -aG docker $USER
-newgrp docker
-
-
-echo "--------------------------------------- DOCKER VERSION ----------------------------------"
-docker --version
-echo "--------------------------------------- DOCKER VERSION ----------------------------------"
-
-
-
-echo "--------------------------------------- DOCKER PS A ----------------------------------"
-docker ps -a
-echo "--------------------------------------- DOCKER PS A ----------------------------------"
-
-echo "--------------------------------------- DOCKER RUN HELLO WORLD ----------------------------------"
-docker run hello-world
-echo "--------------------------------------- DOCKER RUN HELLO WORLD ----------------------------------"
+#apt update && apt -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+#curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/trusted.gpg.d/docker-archive-keyring.gpg
+#add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+#apt update && apt -y install docker-ce docker-ce-cli containerd.io
+#systemctl enable --now docker
+#usermod -aG docker $USER
+#newgrp docker
+#
+#
+#echo "--------------------------------------- DOCKER VERSION ----------------------------------"
+#docker --version
+#echo "--------------------------------------- DOCKER VERSION ----------------------------------"
+#
+#
+#
+#echo "--------------------------------------- DOCKER PS A ----------------------------------"
+#docker ps -a
+#echo "--------------------------------------- DOCKER PS A ----------------------------------"
+#
+#echo "--------------------------------------- DOCKER RUN HELLO WORLD ----------------------------------"
+#docker run hello-world
+#echo "--------------------------------------- DOCKER RUN HELLO WORLD ----------------------------------"
 
 # setting env variables
 export MAVEN_OPTS="-Xms16g -Xmx24g"
