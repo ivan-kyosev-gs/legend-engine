103e8607dd2846f9b39414acdead945bab289a91
Ivan Kyosev <89931850+ivan-kyosev-gs@users.noreply.github.com>
2022-09-20 15:50:11 +0100

    Changing maven core dep version

--- .gcp/cloud-build-release/release.sh
+++ .gcp/cloud-build-release/release.sh
@@ -15,6 +15,11 @@ export MAVEN_OPTS="-Xms16g -Xmx24g"
 git clone https://github.com/kevin-m-knight-gs/legend-pure.git
 cd legend-pure
 git checkout thread_safe_repos
+
+cd legend-pure-maven-java-compiled
+mvn versions:use-dep-version -Dincludes=org.apache.maven:maven-core -DdepVersion=3.8.6 -DforceVersion=true
+cd ..
+
 mvn -B -e -pl !legend-pure-ide-light install
 
 cd ..
