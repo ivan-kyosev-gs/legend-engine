77f396efbfdff863df9e03398911b5c7d06174f4
Ivan Kyosev <89931850+ivan-kyosev-gs@users.noreply.github.com>
2022-09-20 15:14:09 +0100

    Building legend-pure branch

--- .gcp/cloud-build-release/release.sh
+++ .gcp/cloud-build-release/release.sh
@@ -10,5 +10,19 @@ gpg --version
 # setting env variables
 export MAVEN_OPTS="-Xms16g -Xmx24g"
 
+
+
+git clone https://github.com/kevin-m-knight-gs/legend-pure.git
+cd legend-pure
+git checkout thread_safe_repos
+mvn -B -e -pl !legend-pure-ide-light install
+
+cd ..
+
+sed -i 's/${legend.pure.version}/3.6.2-SNAPSHOT/g' pom.xml
+
+
+echo "------------------------------ running legend-engine-build -----------------"
+
 # running the build
 mvn -B -e -T 8 install
